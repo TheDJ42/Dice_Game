e98dbdf7f20029908c66175771bf50baf0cd1b2b
TheDJ42 <dubuntu2sphynxmail.com>
2016-02-04 09:04:16 -0700

    Started getting things to work

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -20,9 +20,42 @@ namespace Dice_Game
     /// </summary>
     public partial class MainWindow : Window
     {
+        int round = 1;
+        int score = 0;
+        int round_score = 0;
         public MainWindow()
         {
+            
             InitializeComponent();
+        }
+
+        private void new_round_Click(object sender, RoutedEventArgs e)
+        {
+            // Update round value and end game after ten
+            if (round > 10)
+            {
+                round_number.Text = "Game Over";
+                die0.IsEnabled = false;
+                die1.IsEnabled = false;
+                die2.IsEnabled = false;
+                die3.IsEnabled = false;
+                new_round.IsEnabled = false;
+
+            }
+            else
+            {
+                round = round + 1;
+                round_number.Text = "Round " + Convert.ToString(round);
+                
+                // Bank score (Add dice together)
+                score = score = round_score;
+            }
+
+
+
+
+
+            // Reroll all dice for next round
         }
     }
 }
