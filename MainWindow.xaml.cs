19cf285f40494c1fba7251e103f33afaaf913413
TheDJ42 <dubuntu2sphynxmail.com>
2016-02-08 08:56:07 -0700

    Most of the things work. Kinda....

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -20,20 +20,48 @@ namespace Dice_Game
     /// </summary>
     public partial class MainWindow : Window
     {
-        Random RNG =
+        Random rnd = new Random();
         int round = 1;
-        int score = 0;
+        int total_score = 0;
         int round_score = 0;
+        int score1 = 0;       
+        int score2 = 0;       
+        int score3 = 0;       
+        int score4 = 0;
+        int rolls_left_num = 5;     
         public MainWindow()
         {
             
             InitializeComponent();
         }
+        private void die1_Click(object sender, RoutedEventArgs e)
+        {
+            score1 = rnd.Next(1, 7);
+            die1.Content = score1.ToString();
+        }
+
+        private void die2_Click(object sender, RoutedEventArgs e)
+        {
+            score2 = rnd.Next(1, 7);
+            die2.Content = score2.ToString();
+        }
+
+        private void die3_Click(object sender, RoutedEventArgs e)
+        {
+            score3 = rnd.Next(1, 7);
+            die3.Content = score3.ToString();
+        }
+
+        private void die0_Click(object sender, RoutedEventArgs e)
+        {
+            score4 = rnd.Next(1, 7);
+            die0.Content = score4.ToString();
+        }
 
         private void new_round_Click(object sender, RoutedEventArgs e)
         {
             // Update round value and end game after ten
-            if (round > 10)
+            if (round >= 10)
             {
                 round_number.Text = "Game Over";
                 die0.IsEnabled = false;
@@ -47,16 +75,17 @@ namespace Dice_Game
             {
                 round = round + 1;
                 round_number.Text = "Round " + Convert.ToString(round);
-                
+
                 // Bank score (Add dice together)
-                //score = score = round_score;
+                round_score = score1 + score2 + score3 + score4;
+                round_score_text.Text = "Round Score: " + round_score.ToString();
+                total_score = total_score + round_score;
+                Score.Text = "Total Score: " + total_score.ToString();
             }
 
-
-
-
-
-            // Reroll all dice for next round
+                    // Reroll all dice for next round
         }
-    }
+
+        
+    }      
 }
