526aebe70d2b07497bc1f246ef96918f563882a2
TheDJ42 <dubuntu2sphynxmail.com>
2016-02-12 08:47:19 -0700

    Rolls left is now working which is wonderful. Also I fixed some formatting so that's good.

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -22,8 +22,8 @@ namespace Dice_Game
     {
         Random rnd = new Random();
         int round = 1;
-        int total_score = 0;
-        int round_score = 0;
+        int totalScore = 0;
+        int roundScore = 0;
         int score1 = 0;       
         int score2 = 0;       
         int score3 = 0;       
@@ -36,36 +36,54 @@ namespace Dice_Game
         }
         private void die1_Click(object sender, RoutedEventArgs e)
         {
-            score1 = rnd.Next(1, 7);
-            die1.Content = score1.ToString();
-            rolls_left_num--;
-            rolls_left.Text = "Rolls Left this round: " + rolls_left_num.ToString();
+            if (rolls_left_num > 0)
+
+            {
+                score1 = rnd.Next(1, 7);
+                die1.Content = score1.ToString();
+                rolls_left_num--;
+                rollsLeft.Text = "Rolls Left this round: " + rolls_left_num.ToString();
+            }
+
+
         }
 
         private void die2_Click(object sender, RoutedEventArgs e)
         {
-            score2 = rnd.Next(1, 7);
-            die2.Content = score2.ToString();
-            rolls_left_num--;
-            rolls_left.Text = "Rolls Left this round: " + rolls_left_num.ToString();
+            if (rolls_left_num > 0)
+
+            {
+                score2 = rnd.Next(1, 7);
+                die2.Content = score2.ToString();
+                rolls_left_num--;
+                rollsLeft.Text = "Rolls Left this round: " + rolls_left_num.ToString();
+            }
 
         }
 
         private void die3_Click(object sender, RoutedEventArgs e)
         {
-            score3 = rnd.Next(1, 7);
-            die3.Content = score3.ToString();
-            rolls_left_num--;
-            rolls_left.Text = "Rolls Left this round: " + rolls_left_num.ToString();
+            if (rolls_left_num > 0)
+
+            {
+                score3 = rnd.Next(1, 7);
+                die3.Content = score3.ToString();
+                rolls_left_num--;
+                rollsLeft.Text = "Rolls Left this round: " + rolls_left_num.ToString();
+            }
         }
         
       
         private void die0_Click(object sender, RoutedEventArgs e)
         {
-            score4 = rnd.Next(1, 7);
-            die0.Content = score4.ToString();
-            rolls_left_num--;
-            rolls_left.Text = "Rolls Left this round: " + rolls_left_num.ToString();
+            if (rolls_left_num > 0)
+
+            {
+                score4 = rnd.Next(1, 7);
+                die0.Content = score4.ToString();
+                rolls_left_num--;
+                rollsLeft.Text = "Rolls Left this round: " + rolls_left_num.ToString();
+            }
         }
 
         private void new_round_Click(object sender, RoutedEventArgs e)
@@ -73,24 +91,24 @@ namespace Dice_Game
             // Update round value and end game after ten
             if (round >= 10)
             {
-                round_number.Text = "Game Over";
+                roundNumber.Text = "Game Over";
                 die0.IsEnabled = false;
                 die1.IsEnabled = false;
                 die2.IsEnabled = false;
                 die3.IsEnabled = false;
-                new_round.IsEnabled = false;
+                newRound.IsEnabled = false;
 
             }
             else
             {
                 round = round + 1;
-                round_number.Text = "Round " + Convert.ToString(round);
+                roundNumber.Text = "Round " + Convert.ToString(round);
 
                 // Bank score (Add dice together)
-                round_score = score1 + score2 + score3 + score4;
-                round_score_text.Text = "Round Score: " + round_score.ToString();
-                total_score = total_score + round_score;
-                Score.Text = "Total Score: " + total_score.ToString();
+                roundScore = score1 + score2 + score3 + score4;
+                roundScore_text.Text = "Round Score: " + roundScore.ToString();
+                totalScore = totalScore + roundScore;
+                score.Text = "Total Score: " + totalScore.ToString();
             }
 
                     // Reroll all dice for next round
