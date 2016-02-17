aa859d1ac4dc59ac293c78ed38fabddb116abd64
TheDJ42 <dubuntu2sphynxmail.com>
2016-02-17 08:15:34 -0700

    The Randomness works and stuff. And it is almost done. So yeah.... Don't steal my source code. :)

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -28,11 +28,27 @@ namespace Dice_Game
         int score2 = 0;       
         int score3 = 0;       
         int score4 = 0;
-        int rolls_left_num = 5;     
+        int rolls_left_num = 5;
+        int die0Val = 0;
+        int die1Val = 0;
+        int die2Val = 0;
+        int die3Val = 0;
+
+
         public MainWindow()
         {
             
             InitializeComponent();
+            die0Val = rnd.Next(1, 7);
+            die1Val = rnd.Next(1, 7);
+            die2Val = rnd.Next(1, 7);
+            die3Val = rnd.Next(1, 7);
+            die0.Content = die0Val.ToString();
+            die1.Content = die1Val.ToString();
+            die2.Content = die2Val.ToString();
+            die3.Content = die3Val.ToString();
+
+
         }
         private void die1_Click(object sender, RoutedEventArgs e)
         {
@@ -97,10 +113,12 @@ namespace Dice_Game
                 die2.IsEnabled = false;
                 die3.IsEnabled = false;
                 newRound.IsEnabled = false;
-
+                
             }
             else
             {
+                rolls_left_num = 5;
+                rollsLeft.Text = "Rolls Left this round: " + rolls_left_num.ToString();
                 round = round + 1;
                 roundNumber.Text = "Round " + Convert.ToString(round);
 
@@ -109,9 +127,20 @@ namespace Dice_Game
                 roundScore_text.Text = "Round Score: " + roundScore.ToString();
                 totalScore = totalScore + roundScore;
                 score.Text = "Total Score: " + totalScore.ToString();
+
+                // Reroll all dice for next round
+                die0Val = rnd.Next(1, 7);
+                die1Val = rnd.Next(1, 7);
+                die2Val = rnd.Next(1, 7);
+                die3Val = rnd.Next(1, 7);
+                die0.Content = die0Val.ToString();
+                die1.Content = die1Val.ToString();
+                die2.Content = die2Val.ToString();
+                die3.Content = die3Val.ToString();
+
             }
 
-                    // Reroll all dice for next round
+
         }
 
         
